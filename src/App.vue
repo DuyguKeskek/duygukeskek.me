556d9d153c563a471a372c7a494f3c1fe678d737
Duygu KESKEK <duygu.keskek@osf-commerce.com>
2020-02-09 18:16:36 +0300

    Implement navigation and routing

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,64 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<v-app id="app">
+		<v-container class="pa-6 pa-lg-12">
+			<v-row id="nav" class="ma-0">
+				<div class="nav-home">
+					<router-link to="/" class="font-weight-bold d-inline-block mr-sm-10 mb-4 mb-sm-0">Duygu Keskek</router-link>
+				</div>
+				<router-link to="/about" class="mr-5">About</router-link>
+				<router-link to="/thoughts" class="mr-5">Thoughts</router-link>
+				<router-link to="/resume" class="mr-5">Resume</router-link>
+				<router-link to="/contact">Contact</router-link>
+			</v-row>
+			<router-view/>
+		</v-container>
+	</v-app>
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Raleway:400,500,700&display=swap');
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: 'Raleway', Helvetica, Arial, sans-serif;
+	height: 100%;
+	font-weight: 300;
+	font-size: 17px;
+	line-height: 1.5;
+	color: #444;
+
+	::selection { 
+		background-color: #0ff;
+		color: #000;
+	}
+}
+
+a {
+	background-repeat: no-repeat;
+	background-size: 100% 1px;
+	background-position: center bottom;
+	text-decoration: none;
 }
 
 #nav {
-  padding: 30px;
+	a {
+		color: #000;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+		&:not(.nav-home) {
+			&:not(:first-child) {
+				&.router-link-exact-active {
+					background-image: linear-gradient(to right,#ee65de,#26cbff);
+				}
+			}
+		}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+		&:hover {
+			background-image: linear-gradient(to right,#000,#000);
+		}
+	}
+
+	.nav-home {
+		@media only screen and (max-width: 600px) {
+			width: 100%;
+		}
+	}
 }
 </style>
-
