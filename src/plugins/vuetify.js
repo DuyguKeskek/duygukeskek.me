27c61976401cbd61b0e121be190eb72a1e86b64b
DuyguKeskek <duygukeskek@hotmail.com>
2020-09-24 01:33:16 +0300

    Refactor the page title and fix indentations

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,10 +2,10 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
     icons: {
         iconfont: 'fa'
     }
-});
+})
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home'
+import About from '../views/About'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+        meta: { title: 'Home' }
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About,
+        meta: { title: 'About' }
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    const pageTitle = 'Duygu Keskek'
+    document.title = to.meta.title ? pageTitle + ' - ' + to.meta.title : pageTitle
+    next()
 })
 
 export default router
